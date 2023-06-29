04b10ede7fde572cae1c1c826fe30be9fce757b4
Benjamin Reed <ben.reed@sjsu.edu>
2023-06-28 20:09:05 -0700

    quick fix to opaque token resolver (#38)
    we need to resolve the tokens asynchronously, but ReactiveOpaqueTokenResolver doesn't work.
    this is a quick fix.

--- server/src/main/java/edu/sjsu/moth/server/util/CustomAuthoritiesOpaqueTokenIntrospector.java
+++ server/src/main/java/edu/sjsu/moth/server/util/CustomAuthoritiesOpaqueTokenIntrospector.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.DefaultOAuth2AuthenticatedPrincipal;
 import org.springframework.security.oauth2.core.OAuth2AuthenticatedPrincipal;
-import org.springframework.security.oauth2.server.resource.introspection.ReactiveOpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
 import reactor.core.publisher.Mono;
 
 import java.util.Collection;
@@ -16,16 +16,12 @@ import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
-public class CustomAuthoritiesOpaqueTokenIntrospector implements ReactiveOpaqueTokenIntrospector {
+public class CustomAuthoritiesOpaqueTokenIntrospector implements OpaqueTokenIntrospector {
     @Autowired
     TokenRepository tokenRepository;
 
-    public Mono<OAuth2AuthenticatedPrincipal> introspect(String token) {
+    public OAuth2AuthenticatedPrincipal introspect(String token) {
         return tokenRepository.findItemByToken(token)
-                .map(t -> new DefaultOAuth2AuthenticatedPrincipal(t.user, Map.of("sub", t.user), Set.of()));
-    }
-
-    private Collection<GrantedAuthority> extractAuthorities(OAuth2AuthenticatedPrincipal principal) {
-        return Set.of();
+                .map(t -> new DefaultOAuth2AuthenticatedPrincipal(t.user, Map.of("sub", t.user), Set.of())).block();
     }
 }
