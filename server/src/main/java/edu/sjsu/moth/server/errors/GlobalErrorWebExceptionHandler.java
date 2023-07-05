484b08699d1d03c755ec9e0138c90a1c9b79f2fc
ben reed <ben.reed@sjsu.edu>
2023-07-05 16:16:22 -0700

    added logging of error
    we lost error logging when we completely switched to webflux

---
+++ server/src/main/java/edu/sjsu/moth/server/errors/GlobalErrorWebExceptionHandler.java
@@ -0,0 +1,46 @@
+package edu.sjsu.moth.server.errors;
+
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.reactive.error.ErrorAttributes;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.*;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Component
+@Order(-2)
+public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
+    public GlobalErrorWebExceptionHandler(ErrorAttributes errorAttributes, WebProperties.Resources resources,
+                                          ApplicationContext applicationContext, ServerCodecConfigurer configurer) {
+        super(errorAttributes, resources, applicationContext);
+        this.setMessageReaders(configurer.getReaders());
+        this.setMessageWriters(configurer.getWriters());
+    }
+
+    // constructors
+
+    @Override
+    protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
+
+        return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
+    }
+
+    private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
+
+        Map<String, Object> errorPropertiesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+
+        System.out.println("Got errors: " + errorPropertiesMap);
+        return ServerResponse.status(HttpStatus.BAD_REQUEST)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(errorPropertiesMap));
+    }
+}
