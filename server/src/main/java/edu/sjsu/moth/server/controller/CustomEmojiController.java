89a3ca53bff5a7c80a97b2a548742c4728c7bd2f
CheahWH <78494351+CheahWH@users.noreply.github.com>
2023-07-06 15:28:42 -0700

    deleted unnecessary methods/queries and CustomEmojiDB.java.

--- server/src/main/java/edu/sjsu/moth/server/controller/CustomEmojiController.java
+++ server/src/main/java/edu/sjsu/moth/server/controller/CustomEmojiController.java
@@ -1,7 +1,8 @@
 package edu.sjsu.moth.server.controller;
 
 import edu.sjsu.moth.server.db.CustomEmoji;
-import edu.sjsu.moth.server.db.CustomEmojiDB;
+import edu.sjsu.moth.server.db.CustomEmojiRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -10,14 +11,16 @@ import reactor.core.publisher.Flux;
 public class CustomEmojiController {
     //implement logic to fetch and return the custom emojis from the server
     //use a repository to handle the data retrieval!
-    private CustomEmojiDB customEmojiDB;
+    private final CustomEmojiRepository customEmojiRepository;
 
-    public CustomEmojiController(CustomEmojiDB customEmojiDB) {
-        this.customEmojiDB = customEmojiDB;
+    @Autowired
+    public CustomEmojiController(CustomEmojiRepository customEmojiRepository) {
+        this.customEmojiRepository = customEmojiRepository;
     }
+
     //https://docs.joinmastodon.org/methods/custom_emojis/#get
     @GetMapping("/api/v1/custom_emojis")
     public Flux<CustomEmoji> getAllCustomEmojis() {
-        return customEmojiDB.getAllCustomEmojis();
+        return customEmojiRepository.findAll();
     }
 }
