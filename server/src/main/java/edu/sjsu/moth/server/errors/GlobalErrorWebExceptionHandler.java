7d87b4a6c9f32f7eed11767e19acd8f7d6638782
ben reed <ben.reed@sjsu.edu>
2023-07-05 19:20:30 -0700

    concise global error logging
    concisingly logs errors, only printing stack traces that have our code.
    add @CommongLog annotation
    fix import limit to 999 to avoid import *

--- server/src/main/java/edu/sjsu/moth/server/errors/GlobalErrorWebExceptionHandler.java
+++ server/src/main/java/edu/sjsu/moth/server/errors/GlobalErrorWebExceptionHandler.java
@@ -1,21 +1,27 @@
 package edu.sjsu.moth.server.errors;
 
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 import org.springframework.boot.autoconfigure.web.reactive.error.AbstractErrorWebExceptionHandler;
 import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.ErrorAttributes;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.codec.ServerCodecConfigurer;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.*;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.RouterFunctions;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.Map;
 
+@CommonsLog
 @Component
 @Order(-2)
 public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHandler {
@@ -26,7 +32,14 @@ public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHan
         this.setMessageWriters(configurer.getWriters());
     }
 
-    // constructors
+    /*
+     * when logging the stack trace, only log stack frames from our code.
+     */
+    private static void filterStack(StringBuilder logBuilder, Throwable cause) {
+        Arrays.stream(cause.getStackTrace()).forEach(stack -> {
+            if (stack.getClassName().startsWith("edu.sjsu")) logBuilder.append("\n        ").append(stack);
+        });
+    }
 
     @Override
     protected RouterFunction<ServerResponse> getRoutingFunction(ErrorAttributes errorAttributes) {
@@ -34,13 +47,37 @@ public class GlobalErrorWebExceptionHandler extends AbstractErrorWebExceptionHan
         return RouterFunctions.route(RequestPredicates.all(), this::renderErrorResponse);
     }
 
+    /*
+     * try to concisely all errors. this is a global logger. for brevity, we only print elements of the
+     * stack that are in our code. (starts with edu.sjsu)
+     */
+    @Override
+    protected void logError(ServerRequest request, ServerResponse response, Throwable throwable) {
+        var errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        errorAttributes.remove("timestamp"); // the log already has one
+        var logBuilder = new StringBuilder("Request error: ").append(errorAttributes);
+        if (throwable != null) {
+            logBuilder.append(" exception: ").append(throwable.getMessage());
+            var cause = throwable.getCause();
+            if (cause != null) {
+                logBuilder.append(" caused by ").append(cause.getMessage());
+                filterStack(logBuilder, cause);
+            } else {
+                filterStack(logBuilder, throwable);
+            }
+        }
+        log.warn(logBuilder.toString());
+    }
+
+    /*
+     * we are going to render the error response as Json
+     */
     private Mono<ServerResponse> renderErrorResponse(ServerRequest request) {
-
-        Map<String, Object> errorPropertiesMap = getErrorAttributes(request, ErrorAttributeOptions.defaults());
-
-        System.out.println("Got errors: " + errorPropertiesMap);
-        return ServerResponse.status(HttpStatus.BAD_REQUEST)
+        var errorAttributes = getErrorAttributes(request, ErrorAttributeOptions.defaults());
+        return ServerResponse.status((Integer) errorAttributes.get("status"))
                 .contentType(MediaType.APPLICATION_JSON)
-                .body(BodyInserters.fromValue(errorPropertiesMap));
+                .body(BodyInserters.fromValue(
+                        Map.of("path", errorAttributes.getOrDefault("path", "missing path"), "detail",
+                               errorAttributes.getOrDefault("error", "missing error"))));
     }
 }
