03dd87f79b8c217718a5371c68a104575fd7fd50
CheahWH <78494351+CheahWH@users.noreply.github.com>
2023-07-12 14:23:41 -0700

    removed autowire

--- server/src/test/java/edu/sjsu/moth/controllers/InstanceControllerTest.java
+++ server/src/test/java/edu/sjsu/moth/controllers/InstanceControllerTest.java
@@ -5,7 +5,6 @@ import edu.sjsu.moth.server.controller.InstanceController;
 import edu.sjsu.moth.server.db.AccountRepository;
 import edu.sjsu.moth.server.util.MothConfiguration;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContextInitializer;
@@ -24,11 +23,11 @@ public class InstanceControllerTest {
 
     @MockBean
     AccountRepository accountRepository;
-    @Autowired
+
     private WebTestClient webTestClient;
 
     @Test
-    public void testGetRules() {
+    public void testGetRules(WebTestClient webTestClient) {
         webTestClient.get()
                 .uri("/rules")
                 .exchange()
