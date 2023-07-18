dcf0789fa500862e6f588c3ffa46ad588b359616
Angela Huang <angelahuang@MacBook-Pro.local>
2023-07-17 21:08:53 -0700

    added followers

--- server/src/main/java/edu/sjsu/moth/server/controller/InboxController.java
+++ server/src/main/java/edu/sjsu/moth/server/controller/InboxController.java
@@ -1,13 +1,19 @@
 package edu.sjsu.moth.server.controller;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
+import edu.sjsu.moth.server.db.Followers;
 import edu.sjsu.moth.server.db.FollowersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.beans.support.PagedListHolder.DEFAULT_PAGE_SIZE;
 
 @RestController
 public class InboxController {
@@ -34,10 +40,11 @@ public class InboxController {
         String follower = inboxNode.get("actor").asText();
         if (requestType.equals("Follow")) {
             // find id, grab arraylist, append
-            return followersRepository.findItemById(id).flatMap(followedUser -> {
-                followedUser.getFollowers().add(follower);
-                return followersRepository.save(followedUser).thenReturn("done");
-            });
+            return followersRepository.findItemById(id).switchIfEmpty(Mono.just(new Followers(id, new ArrayList<>())))
+                    .flatMap(followedUser -> {
+                        followedUser.getFollowers().add(follower);
+                        return followersRepository.save(followedUser).thenReturn("done");
+                    });
         }
         if (requestType.equals("Undo")) {
             // find id, grab arraylist, remove
@@ -48,4 +55,49 @@ public class InboxController {
         }
         return Mono.empty();
     }
+
+    @GetMapping("/users/{id}/followers")
+    public Mono<?> usersFollowers(@PathVariable String id, @RequestParam(required = false) Integer page, @RequestParam(required = false) Integer limit) {
+        var context = getContext();
+        var items = followersRepository.findItemById(id).map(Followers::getFollowers);
+        String returnID = MothController.BASE_URL + "/users/" + id + "/followers";
+        int pageSize = limit != null ? limit : DEFAULT_PAGE_SIZE;
+        if(page == null) {
+            String first = returnID + "?page=1";
+            return items.map(v -> new Response1(context, returnID, "OrderedCollection", v.size(), first));
+        } else { // page number is given
+            return items.map(v -> {
+                if(page*pageSize >= v.size()) { // no next page
+                    return new Response2(context, returnID, "OrderedCollectionPage", v.size(), returnID, paginateFollowers(v, page, pageSize));
+                } else {
+                    String next = returnID + "?page=" + (page+1);
+                    return new Response3(context, returnID, "OrderedCollectionPage", v.size(), next, returnID, paginateFollowers(v, page, pageSize));
+                }
+            });
+        }
+    }
+
+    public List<String> paginateFollowers(ArrayList<String> followers, int pageNo, int pageSize) {
+        int startIndex = (pageNo - 1) * pageSize;
+        int endIndex = Math.min(startIndex + pageSize, followers.size());
+        if (startIndex >= followers.size()) {
+            return Collections.emptyList();
+        }
+        endIndex = Math.min(endIndex, followers.size());
+        return followers.subList(startIndex, endIndex);
+    }
+
+    public record Response1(String context, String id, String type, int totalItems, String first) {}
+
+    // if a page number is given
+    public record Response2(String context, String id, String type, int totalItems, String partOf, List<String> orderedItems) {}
+
+    // if there is a next follower page
+    public record Response3(String context, String id, String type, int totalItems, String next, String partOf, List<String> orderedItems) {}
+
+    @JsonProperty("@context")
+    public String getContext() {
+        return "https://www.w3.org/ns/activitystreams";
+    }
+
 }
--- server/src/main/java/edu/sjsu/moth/server/db/FollowersRepository.java
+++ server/src/main/java/edu/sjsu/moth/server/db/FollowersRepository.java
@@ -1,11 +1,16 @@
 package edu.sjsu.moth.server.db;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface FollowersRepository extends ReactiveMongoRepository<Followers, String>{
     @Query("{id:'?0'}")
     Mono<Followers> findItemById(String id);
+
+    @Query("{id:'?0'}")
+    Flux<Followers> findItemByIdAndPage(String id, Pageable pageable);
 
 }
