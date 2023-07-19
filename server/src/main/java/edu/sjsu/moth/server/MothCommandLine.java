d8bdae0f50b40113a4609eb52aacc281281519ff
Andre Manzo <drecollege1@gmail.com>
2023-07-19 11:00:12 -0700

    added --verify to terminal in picocli. It checks of the file exists then loads the data

--- server/src/main/java/edu/sjsu/moth/server/MothCommandLine.java
+++ server/src/main/java/edu/sjsu/moth/server/MothCommandLine.java
@@ -36,23 +36,20 @@ public class MothCommandLine implements Runnable {
     public void run() {
         final var prefix = "spring.";
         try {
-
-            if(verification){
+            if (verification) {
                 File f = new File(configFile.toURI());
-                if(f.isFile()) {
-
+                if (f.isFile()) {
                     FileInputStream fileInputStream = new FileInputStream(f);
                     properties.load(fileInputStream);
                     System.out.println("VERIFIED");
 
                     System.exit(1);
-                }
-                else
-                {
+                } else {
                     System.out.println("File not found, Please input proper configuration file");
                     System.exit(1);
                 }
             }
+
 
 
             MothConfiguration config;
