5c1e52b3d14aba7ffd71282861e6822e400dda5f
surelyli <shirley.li@sjsu.edu>
2023-09-05 16:26:13 -0700

    Fixed formatting and installed 17

--- server/src/main/java/edu/sjsu/moth/server/controller/AccountController.java
+++ server/src/main/java/edu/sjsu/moth/server/controller/AccountController.java
@@ -13,7 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -122,6 +121,5 @@ public class AccountController {
         // it's not clear what we need to do with the user...
         return accountService.getAccount(id).map(ResponseEntity::ok);
     }
-
 
 }
