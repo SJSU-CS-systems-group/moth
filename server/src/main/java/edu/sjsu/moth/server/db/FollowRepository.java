92eadbd068f9428ad569fccc6ec09e9bad7d2560
Nishanth Uchil <nishanthuchil@ShanslaoftheM1s.attlocal.net>
2024-05-05 16:13:48 -0700

    fixed camel case naming in query annotation

--- server/src/main/java/edu/sjsu/moth/server/db/FollowRepository.java
+++ server/src/main/java/edu/sjsu/moth/server/db/FollowRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import reactor.core.publisher.Mono;
 
 public interface FollowRepository extends ReactiveMongoRepository<Follow, Follow.FollowKey> {
-    @Query("{'id.followedId': ?0}")
+    @Query("{'id.followed_id': ?0}")
     Mono<List<Follow>> findAllByFollowedId(String followed_id);
-    @Query("{'id.followerId': ?0}")
+    @Query("{'id.follower_id': ?0}")
     Mono<List<Follow>> findAllByFollowerId(String follower_id);
 //    Mono<Integer> countAllByIdFollowedId(String followed_id);
 //    Mono<Integer> countAllByIdFollowerId(String follower_id);
