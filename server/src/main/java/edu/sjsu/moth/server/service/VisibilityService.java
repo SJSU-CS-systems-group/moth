a4e10f77d04566b78b8679277b5dbac24aa94f67
Thinh Bui <thinh.ducbui@gmail.com>
2025-04-28 09:38:41 -0700

    remove unused method and member

--- server/src/main/java/edu/sjsu/moth/server/service/VisibilityService.java
+++ server/src/main/java/edu/sjsu/moth/server/service/VisibilityService.java
@@ -3,6 +3,7 @@ package edu.sjsu.moth.server.service;
 import edu.sjsu.moth.generated.Status;
 import edu.sjsu.moth.server.controller.MothController;
 import edu.sjsu.moth.server.db.FollowRepository;
+import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -13,15 +14,8 @@ import java.security.Principal;
 import java.util.logging.Logger;
 
 @Service
+@CommonsLog
 public class VisibilityService {
-    Logger LOG = Logger.getLogger(VisibilityService.class.getName());
-
-    @Autowired
-    AccountService accountService;
-
-    @Autowired
-    FollowRepository followRepository;
-
     final String PUBLIC_VISIBILITY = "public";
     final String QUITE_PUBLIC = "unlisted";
     final String DIRECT_VISIBILITY = "direct";
@@ -32,7 +26,4 @@ public class VisibilityService {
         return Flux.empty();
     }
 
-    public boolean homefeedViewable(Principal user, Status status) { return false; }
-    public boolean profileViewable(Principal user, Status status) { return false; };
-    public boolean permalinkViewable(Principal user, Status status) { return false; };
 }
